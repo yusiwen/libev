77e1818c2f0b4964bea501de3e639ea671135d70
root <root>
2012-04-12 04:10:15 +0000

    *** empty log message ***

--- ev.c
+++ ev.c
@@ -581,6 +581,8 @@ struct signalfd_siginfo
       #define ECB_MEMORY_FENCE         __asm__ __volatile__ ("bcr 15,0" : : : "memory")
     #elif defined __mips__
       #define ECB_MEMORY_FENCE         __asm__ __volatile__ ("sync" : : : "memory")
+    #elif defined __alpha__
+      #define ECB_MEMORY_FENCE         __asm__ __volatile__ ("mb" : : : "memory")
     #endif
   #endif
 #endif
