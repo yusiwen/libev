98eaa9418102e23ae383acc544aaf1d19d54eccf
root <root>
2020-01-24 13:21:28 +0000

    *** empty log message ***

--- ev.h
+++ ev.h
@@ -215,7 +215,7 @@ struct ev_loop;
 /*****************************************************************************/
 
 #define EV_VERSION_MAJOR 4
-#define EV_VERSION_MINOR 31
+#define EV_VERSION_MINOR 32
 
 /* eventmask, revents, events... */
 enum {
